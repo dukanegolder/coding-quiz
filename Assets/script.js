6d153a88f450f4d7a815852535d6eccde08c41f7
dukanegolder <dukanegolder@gmail.com>
2023-01-06 16:09:32 -0700

    initial commit

---
+++ Assets/script.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        question: 'Which one of these is a boolean?',
+        choices: ['String', 'True', 'Number', 'Undefined'],
+        correct: 'True'
+    },
+    {
+        question: 'What symbole is used to create a class element in CSS?',
+        choices: ['@', '*', '#', '.'],
+        correct: '.'
+    },
+    {
+        question: 'What does JS stand for?',
+        choices: ['Juicestand', 'Javascript', 'Javastring', 'Justscript'],
+        correct: 'Javascript'
+    },
+    {
+        question: 'Javascript is the same as Java?',
+        choices: ['False', 'True'],
+        correct: 'true'
+    },
+]
+
+var homepage = document.querySelector(".rules")
+var startbtn = document.getElementById('#startbtn')
+
+startbtn.addEventListener("click", function () {
+
+})
